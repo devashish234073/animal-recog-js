75c1832fe415a587c6bbbdd6433cfbbbb20ef782
Devashish Priyadarshi <devashish234073@gmail.com>
2021-05-02 23:38:52 +0530

    modified index.js and added package.json

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var http = require("http");
 var fs = require("fs");
-var PORT = 8080;
+var PORT = 8088;
 
 function serverFunction(req,res) {
+  res.writeHead(200,{'Content-Type':'text/html'});
   if(req.url=="/"){
     fs.readFile("toruses.html",(err,data)=>{
       res.end(String(data));
@@ -13,5 +14,5 @@ function serverFunction(req,res) {
 var server = http.createServer(serverFunction);
 
 server.listen(PORT,()=>{
-  console.log("started listening on port ");
+  console.log(`started listening on port ${PORT}`);
 });
